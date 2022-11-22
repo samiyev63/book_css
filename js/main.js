1291aea3ad9d824593a2c61ce142c582aaf697d4
shaxzod <samiyevshaxzod616@gmail.com>
2022-11-23 01:06:16 +0500

    book

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// const convert = [
+//   ["name", "Ibrohim"],
+//   ["age", "20"],
+//   ["country", "Uzbekistan"],
+// ];
+
+// const obj = Object.assign({}, convert);
+
+// console.log(obj);
+
+function $(selector) {
+  return document.querySelector(selector);
+}
+const all = [];
+
